4d13fa3f551b6eebe32aeb20fe04258e637d4945
Raghav Krishnet <66867030+Raghav2002365@users.noreply.github.com>
2022-01-22 17:42:50 +0530

    Update index.html

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@ $subject= $_POST['subject'];
 $to = "raghvendrapratapyadav23@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message ."\r\n Mobile number =" $number;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
+$headers = "From: yraghav152@gmail.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
